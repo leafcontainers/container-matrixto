acc60dd963bd2d3c55375f4e1c28d323b4ef7643
julia <julia@volcanocoffee.monster>
2023-06-17 03:29:27 +0800

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -1,16 +1,8 @@
 #!/usr/bin/env bash
 set -ouex pipefail
 
-git clone -b $(git -c 'versionsort.suffix=-' \
-  ls-remote \
-  --exit-code \
-  --refs \
-  --sort='version:refname' \
-  --tags  https://github.com/matrix-org/matrix.to.git '*.*.*' | \
-  tail --lines=1 | \
-  cut --delimiter='/' --fields=3 ) \
-  https://github.com/matrix-org/matrix.to.git \
-  /opt/matrix_to
+mkdir  /opt/matrix_to
+git clone -b $(git -c 'versionsort.suffix=-' ls-remote --exit-code --refs --sort='version:refname' --tags  https://github.com/matrix-org/matrix.to.git '*.*.*' | tail --lines=1 | cut --delimiter='/' --fields=3 ) https://github.com/matrix-org/matrix.to.git /opt/matrix_to
 
 cd /opt/matrix_to
 yarn
