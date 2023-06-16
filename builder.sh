18ab98b5cb590e7b98717cabb1bf2ac5f9a3fa17
julia <julia@volcanocoffee.monster>
2023-06-17 03:36:27 +0800

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -7,5 +7,4 @@ mkdir /opt/matrix_to
 cd /opt/matrix_to
 
 git clone -b $(git -c 'versionsort.suffix=-' ls-remote --exit-code --refs --sort='version:refname' --tags  https://github.com/matrix-org/matrix.to.git '*.*.*' | tail --lines=1 | cut --delimiter='/' --fields=3 ) https://github.com/matrix-org/matrix.to.git /opt/matrix_to
-
 yarn
