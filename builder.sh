e9352cb04bc6c623b9627ff064bd2cafcbb66e36
julia <julia@volcanocoffee.monster>
2023-06-17 03:43:40 +0800

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -ouex pipefail
 
-apk add git
+apk add git bash
 
 mkdir /opt/matrix_to
 cd /opt/matrix_to
-latestTag=$(git -c 'versionsort.suffix=-' ls-remote --exit-code --refs --sort='version:refname' --tags https://github.com/matrix-org/matrix.to.git '*.*.*' | tail --lines=1 | cut --delimiter='/' --fields=3)
-git clone -b $latestTag https://github.com/matrix-org/matrix.to.git /opt/matrix_to
+latestTag="$(git -c 'versionsort.suffix=-' ls-remote --exit-code --refs --sort='version:refname' --tags https://github.com/matrix-org/matrix.to.git '*.*.*' | tail --lines=1 | cut --delimiter='/' --fields=3)"
+bash -c git clone -b $latestTag https://github.com/matrix-org/matrix.to.git /opt/matrix_to
 yarn
