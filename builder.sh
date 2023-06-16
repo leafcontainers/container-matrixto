ad2f45540fb7a4e9dd4f350b7ecee306bc84653d
julia <julia@volcanocoffee.monster>
2023-06-17 01:54:25 +0800

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -5,5 +5,8 @@ apk add git
 
 git clone https://github.com/matrix-org/matrix.to /opt/matrix_to
 cd /opt/matrix_to
+git fetch --tags
+latestTag=$(git describe --tags `git rev-list --tags --max-count=1`)
+git checkout $latestTag
 
 yarn
